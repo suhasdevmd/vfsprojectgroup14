0370942bde1a43cee8d198848cae340bc4f38a6d
dm.project.14@gmail.com <dm.project.14@gmail.com@f6b86b19-81ed-c906-b694-f127c33ed15b>
2012-09-23 19:32:09 +0000

    Suhas. Updated create vfs file. Writing the structures and reading the contents.

--- trunk/src/create_vfs.c
+++ trunk/src/create_vfs.c
@@ -22,8 +22,9 @@ printf("File doesnot exist.\n");
 }
 else
 {
-//allocating 1MB of memory and initializing with 0
-fwrite(a,1,10000000,fp);
+//allocating 1MB of memory 
+fseek(fp,1000000,SEEK_SET);
+fwrite(a,1,1,fp);
 }
 
 
@@ -32,8 +33,8 @@ fwrite(a,1,10000000,fp);
 strcpy(mheader.file_sys_label,"VfsProject");
 mheader.used_file_desc=0;
 mheader.max_file_desc=100;
-struct file_descriptor fd={{0}};
-struct freelist fl={{0}};
+struct file_descriptor fd;
+struct freelist fl;
 mheader.file_desc=fd;
 mheader.free_list=fl;
 
@@ -52,9 +53,11 @@ memset ( f_desc, 0x00, sizeof(f_desc));// for initializing the array of fds' to 
 fseek(fp,sizeof(struct mainheader),SEEK_SET);
 fwrite(&f_desc,sizeof(struct file_descriptor),20,fp);
 
+
+
 //creating the blocks in the memory
 struct block blk[100];
-memset ( blk, 0x00, sizeof(blk));// initializing the blocks to 0
+//memset ( blk, 0x00, sizeof(blk));// initializing the blocks to 0
 
 for(i=0;i<100;i++)
 {
@@ -62,26 +65,30 @@ blk[i].address=1000;
 strcpy(blk[i].buffer,"Block1");
 }
 
-fseek(fp,sizeof(struct mainheader)+(sizeof(struct file_descriptor)*20)+1000,SEEK_CUR);
+
+//fseek(fp,1000,SEEK_SET);
+fseek(fp,sizeof(struct mainheader)+(sizeof(struct file_descriptor)*20),SEEK_SET);
 fwrite(&blk,sizeof(struct block),100,fp);
+
 
 
 
 printf("\nReading the VFS name ---------> \n");
 //reading the contents of the file
-char b[100];
+struct block b;
+char m[100];
 FILE *fp1=fopen("/home/suhasdev/Desktop/VfsProject/VFSystem","rb");
-fread(b,1,12,fp1);
-printf("\n%s\n",b);
+fread(m,1,12,fp1);
+printf("\n%s\n",m);
 
 printf("\nReading the block ---------> \n");
-//fp1=fopen("/home/suhasdev/Desktop/VfsProject/VFSystem","rb");
-fseek(fp1,0,SEEK_SET);
-fseek(fp1,sizeof(struct mainheader)+(sizeof(struct file_descriptor)*20)+1000,SEEK_SET);
-fread(b,1,12,fp1);
+fp1=fopen("/home/suhasdev/Desktop/VfsProject/VFSystem","rb");
+//fseek(fp1,1000,SEEK_SET);
+fseek(fp1,sizeof(struct mainheader)+(sizeof(struct file_descriptor)*20),SEEK_SET);
+fread(&b,sizeof(struct block),1,fp1);
 //fread(b,1,10000000,fp1);
 
-printf("\n%s\n",b);
+printf("\n%d\n",b.address);
 printf("\nAfter reading block 1 \n");
 
 return 0;
